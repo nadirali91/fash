61fdae4a13761fd40606b7d8d99a5739d547e8c6
Omar Hamada <omarhamada@live.fr>
2017-10-14 16:06:34 +0000

    Added username to db, routes and schema

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,13 @@ class PagesController < ApplicationController
   end
 
   def profile
+    
+    if (User.find_by_username(params[:id]))
+      @username = params[:id]
+    else
+      redirect_to root_path, :notice=> "username not found"
+    end
+    
   end
 
   def explore
